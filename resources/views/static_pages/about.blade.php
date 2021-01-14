4c30a1c92f74139324b8262cbbd5ec23c5c49331
wangwei171 <wangwei171@126.com>
2021-01-14 16:58:29 +0800

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','关于页')
+
+@section('content')
+	<h2>关于页</h2>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','首页')
+
+@section('content')
+	<h2>首页</h2>
+@stop
