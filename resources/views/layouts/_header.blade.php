2a7e4784c4fe8668393509739047f234d9b61fe9
wangwei171 <wangwei171@126.com>
2021-01-21 14:26:23 +0800

    用户登录退出

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,12 +2,37 @@
 	<div class="container">
 		<a class="navbar-brand" href="{{route('home')}}">Weibo App</a>
 		<ul class="navbar-nav justify-content-end">
-			<li class="nav-item">
-				<a class="nav-link" href="{{route('help')}}">帮助</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="#">登录</a>
-			</li>
+
+			@if(Auth::check())
+				<li class="nav-item"><a class="nav-link" href="#">用户列表</a></li>
+
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
+						{{Auth::user()->name}}
+					</a>
+
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a href="{{route('users.show',Auth::user())}}" class="dropdown-item">个人中心</a>
+						<a href="#" class="dropdown-item">编辑资料</a>
+						<div class="dropdown-divider"></div>
+						<a href="#" class="dropdown-item" id="logout">
+							<form method="POST" action="{{route('logout')}}">
+								{{csrf_field()}}
+								{{method_field('DELETE')}}
+								<button type="submit" name="button" class="btn btn-block btn-danger">退出</button>
+							</form>
+						</a>
+					</div>
+				</li>
+
+			@else
+				<li class="nav-item">
+					<a class="nav-link" href="{{route('help')}}">帮助</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">登录</a>
+				</li>
+			@endif
 		</ul>
 	</div>
 </nav>
