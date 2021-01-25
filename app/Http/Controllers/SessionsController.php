94b015ac0dc6dacbac73de00457c0253b9e9538c
wangwei171 <wangwei171@126.com>
2021-01-25 14:41:15 +0800

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\User;
 
 class SessionsController extends Controller
 {
@@ -21,7 +22,8 @@ class SessionsController extends Controller
 
     	if(Auth::attempt($credentials,$request->has('remember'))){
     		session()->flash('success','欢迎回来！');
-    		return redirect()->route('users.show',[Auth::user()]);
+            $fallback = route('users.show',Auth::user());
+    		return redirect()->intended($fallback);
     	}else{
     		session()->flash('danger','您的邮箱和密码不匹配！');
     		return redirect()->back()->withoutInput();
