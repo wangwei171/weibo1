cc87bd75cba17c7a9a021e3da9eab16e4ab8270c
wangwei171 <wangwei171@126.com>
2021-01-26 16:54:25 +0800

    删除用户

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -1,4 +1,15 @@
 <div class="list-group-item">
 	<img class="mr-3" src="{{$user->gravatar()}}" alt="{{$user->name}}" width="32">
-	<a href="route('users.show',$user)">{{$user->name}}</a>				
+	<a href="route('users.show',$user)">{{$user->name}}</a>	
+
+	@can('destroy',$user)
+		<form action="{{route('users.destroy',$user->id)}}" method="POST" class="float-right">
+			{{csrf_field()}}
+			{{method_field('DELETE')}}
+			<button class="btn btn-sm btn-danger delete-btn">删除</button>
+			
+		</form>
+	@endcan
+
 </div>
+
