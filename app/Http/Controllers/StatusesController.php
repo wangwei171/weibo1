b4115b363085a325bc239ba3c21e5ebb39143a5b
wangwei171 <wangwei171@126.com>
2021-02-01 10:55:33 +0800

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\Status;
 
 class StatusesController extends Controller
 {
@@ -26,8 +27,11 @@ class StatusesController extends Controller
     	return redirect()->back();
     }
 
-    public function destroy()
+    public function destroy(Status $status)
     {
-
+        $this->authorize('destroy',$status);
+        $status->delete();
+        session()->flash('success','微博删除成功');
+        return redirect()->back();
     }
 }
